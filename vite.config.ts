5612d5901539fd239cafa450221460dd6055945d
iman malekian <imanmalekian31@gmail.com>
2025-06-09 19:10:07 +0330

    new version

--- vite.config.ts
+++ vite.config.ts
@@ -5,29 +5,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VitePWA({
-      registerType: "autoUpdate",
-      devOptions: {
-        enabled: true,
-      },
-      includeAssets:['src/assets/images/**/*'],
-      manifest: {
-        name: "Sibo",
-        short_name: "Sibo",
-        description: "Sibo",
-        theme_color: "#d1aa4b",
-        icons: [
-          {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          }
-        ],
-      },
-    }),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
